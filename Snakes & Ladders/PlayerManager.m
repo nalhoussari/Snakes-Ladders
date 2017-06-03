8d3e5c26e7b7269ffcf83cc7957433097ea3a5ee
nalhoussari <nalhoussari@hotmail.com>
2017-06-03 15:49:02 -0700

    creating a new class PlayerManager

---
+++ Snakes & Ladders/PlayerManager.m
@@ -0,0 +1,13 @@
+//
+//  PlayerManager.m
+//  Snakes & Ladders
+//
+//  Created by Noor Alhoussari on 2017-06-03.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+#import "PlayerManager.h"
+
+@implementation PlayerManager
+
+@end
